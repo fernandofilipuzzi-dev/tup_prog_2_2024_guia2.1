41bd2bd568293eb85dfb3889a81bac8c1266b0d3
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2024-08-12 22:19:48 -0300

    .

--- Guia2.2/Ejercicio2_liquidacion_de_sueldos/FormPrincipal.Designer.cs
+++ Guia2.2/Ejercicio2_liquidacion_de_sueldos/FormPrincipal.Designer.cs
@@ -43,17 +43,17 @@
             this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(677, 324);
+            this.textBox1.Size = new System.Drawing.Size(640, 324);
             this.textBox1.TabIndex = 0;
             // 
             // btnRegistrarEmpleado
             // 
-            this.btnRegistrarEmpleado.Location = new System.Drawing.Point(596, 7);
+            this.btnRegistrarEmpleado.Location = new System.Drawing.Point(496, 6);
             this.btnRegistrarEmpleado.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnRegistrarEmpleado.Name = "btnRegistrarEmpleado";
             this.btnRegistrarEmpleado.Size = new System.Drawing.Size(131, 52);
             this.btnRegistrarEmpleado.TabIndex = 1;
-            this.btnRegistrarEmpleado.Text = "Registrar Empleado";
+            this.btnRegistrarEmpleado.Text = "Registrar Empleado(1)";
             this.btnRegistrarEmpleado.UseVisualStyleBackColor = true;
             this.btnRegistrarEmpleado.Click += new System.EventHandler(this.btnRegistrarEmpleado_Click);
             // 
@@ -61,7 +61,7 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(703, 92);
+            this.listBox1.Location = new System.Drawing.Point(666, 92);
             this.listBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(302, 324);
@@ -69,23 +69,23 @@
             // 
             // btnGenerar
             // 
-            this.btnGenerar.Location = new System.Drawing.Point(735, 5);
+            this.btnGenerar.Location = new System.Drawing.Point(635, 7);
             this.btnGenerar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnGenerar.Name = "btnGenerar";
-            this.btnGenerar.Size = new System.Drawing.Size(131, 54);
+            this.btnGenerar.Size = new System.Drawing.Size(163, 51);
             this.btnGenerar.TabIndex = 3;
-            this.btnGenerar.Text = "Generar Liquidaciones";
+            this.btnGenerar.Text = "Generar Liquidaciones(2)";
             this.btnGenerar.UseVisualStyleBackColor = true;
             this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
             // 
             // btnMostrarLiquidaciones
             // 
-            this.btnMostrarLiquidaciones.Location = new System.Drawing.Point(874, 5);
+            this.btnMostrarLiquidaciones.Location = new System.Drawing.Point(806, 8);
             this.btnMostrarLiquidaciones.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnMostrarLiquidaciones.Name = "btnMostrarLiquidaciones";
-            this.btnMostrarLiquidaciones.Size = new System.Drawing.Size(131, 53);
+            this.btnMostrarLiquidaciones.Size = new System.Drawing.Size(131, 50);
             this.btnMostrarLiquidaciones.TabIndex = 4;
-            this.btnMostrarLiquidaciones.Text = "Mostrar Liquidaciones";
+            this.btnMostrarLiquidaciones.Text = "Mostrar Liquidaciones(3a)";
             this.btnMostrarLiquidaciones.UseVisualStyleBackColor = true;
             this.btnMostrarLiquidaciones.Click += new System.EventHandler(this.btnMostrarLiquidaciones_Click);
             // 
@@ -96,7 +96,7 @@
             this.btnMostrarReciboSueldo.Name = "btnMostrarReciboSueldo";
             this.btnMostrarReciboSueldo.Size = new System.Drawing.Size(131, 52);
             this.btnMostrarReciboSueldo.TabIndex = 5;
-            this.btnMostrarReciboSueldo.Text = "Mostrar recibo sueldo";
+            this.btnMostrarReciboSueldo.Text = "Mostrar recibo sueldo(3b)";
             this.btnMostrarReciboSueldo.UseVisualStyleBackColor = true;
             this.btnMostrarReciboSueldo.Click += new System.EventHandler(this.btnMostrarReciboSueldo_Click);
             // 
@@ -104,7 +104,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1022, 424);
+            this.ClientSize = new System.Drawing.Size(974, 424);
             this.Controls.Add(this.btnMostrarReciboSueldo);
             this.Controls.Add(this.btnMostrarLiquidaciones);
             this.Controls.Add(this.btnGenerar);
